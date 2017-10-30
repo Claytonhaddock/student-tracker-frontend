0fb5cb8ed87b1769d1c0f08e76e3381dd0feb7b9
Omar Patel <omarpatel123@gmail.com>
2017-10-29 17:54:19 -0700

    add basic framework

--- server.js
+++ server.js
@@ -1,104 +1,77 @@
-//server.js
-'use strict'
-
-//first we import our dependencies...
+// Dependencies
+// ============
 var express = require('express');
-var mongoose = require('mongoose');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser'); // for working with cookies
 var bodyParser = require('body-parser');
-var Comment = require('./model/comments');
+var session = require('express-session'); 
+var methodOverride = require('method-override'); // for deletes in express
 
-//and create our instances
+// Express settings
+// ================
+
+// instantiate our app
 var app = express();
-var router = express.Router();
 
-//set our port to either a predetermined port number if you have set it up, or 3001
-var port = process.env.API_PORT || 3001;
+// override POST to have DELETE and PUT
+app.use(methodOverride('_method'))
 
-//db config -- REPLACE USERNAME/PASSWORD/DATABASE WITH YOUR OWN FROM MLAB!
-mongoose.connect('mongodb://commentuser:password@ds163020.mlab.com:63020/commentdb');
-// mongoose.connect(mongoDB, { useMongoClient: true })
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//allow sessions
+app.use(session(
+	{ 
+		secret: 'app', 
+		cookie: { maxAge: 60000 },
+		resave: true,
+    saveUninitialized: true
+	}
+));
 
-//now we should configure the APi to use bodyParser and look for JSON data in the body
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+//set up handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
+
+require('./routes')(app);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-
-  //and remove cacheing so we get the most recent comments
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-//now  we can set the route path & initialize the API
-router.get('/', function(req, res) {
-  res.json({ message: 'API Initialized!'});
-});
-
-//adding the /comments route to our /api router
-router.route('/comments')
-  //retrieve all comments from the database
-  .get(function(req, res) {
-    //looks at our Comment Schema
-    Comment.find(function(err, comments) {
-      if (err)
-        res.send(err);
-      //responds with a json object of our database comments.
-      res.json(comments)
-    });
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {}
   })
-  //post new comment to the database
-  .post(function(req, res) {
-    var comment = new Comment();
-    (req.body.author) ? comment.author = req.body.author : null;
-    (req.body.text) ? comment.text = req.body.text : null;
-
-    comment.save(function(err) {
-      if (err)
-        res.send(err);
-      res.json({ message: 'Comment successfully added!' });
-    });
-  });
-
-//Adding a route to a specific comment based on the database ID
-router.route('/comments/:comment_id')
-//The put method gives us the chance to update our comment based on the ID passed to the route
-  .put(function(req, res) {
-    Comment.findById(req.params.comment_id, function(err, comment) {
-      if (err)
-        res.send(err);
-      //setting the new author and text to whatever was changed. If nothing was changed
-      // we will not alter the field.
-      (req.body.author) ? comment.author = req.body.author : null;
-      (req.body.text) ? comment.text = req.body.text : null;
-      //save comment
-      comment.save(function(err) {
-        if (err)
-          res.send(err);
-        res.json({ message: 'Comment has been updated' });
-      });
-    });
-  })
-  //delete method for removing a comment from our database
-  .delete(function(req, res) {
-    //selects the comment by its ID, then removes it.
-    Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
-      if (err)
-        res.send(err);
-      res.json({ message: 'Comment has been deleted' })
-    })
-  });
-
-//Use our router configuration when we call /api
-app.use('/api', router);
-
-//starts the server and listens for requests
-app.listen(port, function() {
-  console.log(`api running on port ${port}`);
 });
+
+
+
+// our module get's exported as app.
+module.exports = app;
+
+
+// Where's the listen? Open up bin/www, and read the comments.
